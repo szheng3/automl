b21935e4855c628cbcf45748ad7d75c2a2ee2e57
Sept <sszheng3@gmail.com>
2023-06-17 15:29:02 -0400

    workflow

---
+++ automl.py
@@ -0,0 +1,74 @@
+import logging
+import pandas as pd
+from ludwig.api import LudwigModel
+from ludwig.datasets import agnews
+from ludwig.visualize import confusion_matrix, learning_curves
+
+
+class AgnewsModel:
+
+    def __init__(self, config, dataset=None, logging_level=logging.INFO):
+        self.config = config
+        self.model = LudwigModel(config, logging_level=logging_level)
+        self.train_df, self.test_df, _ = dataset if dataset else agnews.load(split=True)
+        self.train_stats = None
+        self.test_stats = None
+        self.predictions = None
+        self.output_directory = None
+
+    def train(self):
+        self.train_stats, _, self.output_directory = self.model.train(dataset=self.train_df)
+
+    def evaluate(self):
+        self.test_stats, self.predictions, _ = self.model.evaluate(
+            self.test_df, collect_predictions=True, collect_overall_stats=True
+        )
+
+    def predict(self, text_to_predict):
+        self.predictions, _ = self.model.predict(text_to_predict)
+        return self.predictions
+
+
+if __name__ == '__main__':
+
+
+    config = {
+        "input_features": [
+            {
+                "name": "title",
+                "type": "text",
+                "encoder": {
+                    "type": "parallel_cnn"
+                }
+            }
+        ],
+        "output_features": [
+            {
+                "name": "class",
+                "type": "category",
+            }
+        ],
+        "trainer": {
+            "epochs": 3,
+        }
+    }
+
+
+    model = AgnewsModel(config)
+
+    model.train()
+
+    model.evaluate()
+
+
+    text_to_predict = pd.DataFrame({
+        "title": [
+            "Google may spur cloud cybersecurity M&A with $5.4B Mandiant buy",
+            "Europe struggles to meet mounting needs of Ukraine's fleeing millions",
+            "How the pandemic housing market spurred buyer's remorse across America",
+        ]
+    })
+
+    predictions = model.predict(text_to_predict)
+
+    print(predictions)
---
+++ test_automl.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import pytest
+
+from automl import AgnewsModel
+
+# Test configuration
+test_config = {
+    "input_features": [
+        {"name": "title", "type": "text", "encoder": {"type": "parallel_cnn"}}
+    ],
+    "output_features": [{"name": "class", "type": "category"}],
+    "trainer": {"epochs": 1},  # use 1 epoch for test speed
+}
+
+# Test data to predict
+text_to_predict = pd.DataFrame({
+    "title": [
+        "Google may spur cloud cybersecurity M&A with $5.4B Mandiant buy",
+        "Europe struggles to meet mounting needs of Ukraine's fleeing millions",
+        "How the pandemic housing market spurred buyer's remorse across America",
+    ]
+})
+
+def test_model_training():
+    model = AgnewsModel(test_config)
+    model.train()
+    assert model.train_stats is not None
+
+def test_model_evaluation():
+    model = AgnewsModel(test_config)
+    model.train()
+    model.evaluate()
+    assert model.test_stats is not None
+
+def test_model_prediction():
+    model = AgnewsModel(test_config)
+    model.train()
+    predictions = model.predict(text_to_predict)
+    assert predictions is not None
+    assert len(predictions) == len(text_to_predict)
